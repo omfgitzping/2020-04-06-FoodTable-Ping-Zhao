12b16c5c99047664c0538c4037418c09349ed586
Ping Zhao <pzhao11@gmail.com>
2020-04-06 20:27:45 -0700

    Initial Commit

---
+++ 2020-04-06-FoodTable-Ping-Zhao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-04-06-FoodTable-Ping-Zhao
+//
+//  Created by Ping Zhao on 4/6/20.
+//  Copyright © 2020 Ping Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
